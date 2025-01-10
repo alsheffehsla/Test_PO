349968c79e5b403b16a5f82fdbdbaced74adfd59
alshef <alsheffehsla@yandex.ru>
2025-01-10 12:39:32 +0300

    изменено  определение struct в myfunc.c

--- tests/formula_test.h
+++ tests/formula_test.h
@@ -9,22 +9,39 @@
 extern "C" {
 #include "myfunc.h"
 }
+double root_1=0;
+double root_2=0;
+
 
 TEST(formulaTest, a0) {
-    ASSERT_EQ(formula(0,2,4), -222); // Это не квадратное уравнение, так как a = 0
+    formula(0,2,4,&root_1, &root_2);
+    ASSERT_EQ(root_1, -222); // Это не квадратное уравнение, так как a = 0
+    root_1=0;
 }
 
 TEST(formulaTest, 2_roots) {
-    ASSERT_EQ(formula(1,2,3), 0,-2);
-    ASSERT_EQ(formula(2,5,3), 1,-1.5);
+    formula(1,2,3,&root_1, &root_2);
+    ASSERT_EQ(root_1, 0);
+    ASSERT_EQ(root_2,-2);
+    root_1=0;
+    root_2=0;
+    formula(2,5,3,&root_1, &root_2);
+    ASSERT_EQ(root_1, 1);
+    ASSERT_EQ(root_2,-1.5);
+    root_1=0;
+    root_2=0;
 }
 
 TEST(formulaTest, 1_root) {
-    ASSERT_EQ(formula(4,4,1), -2);
+    formula(4,4,1,&root_1, &root_2);
+    ASSERT_EQ(root_1, -2);
+    root_1=0;
 }
 
 TEST(formulaTest, 0_root) {
-    ASSERT_EQ(formula(2,0,1), -111);
+    formula(2,0,1,&root_1, &root_2);
+    ASSERT_EQ(root_1, -111);
+    root_1=0;
 }
 
 #endif // FORMULA_H
