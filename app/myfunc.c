3a716b55fcb39c3dcff863ade9776177f71a28c9
alshef <alsheffehsla@yandex.ru>
2025-01-10 12:49:59 +0300

    struct заменен на указатели

--- app/myfunc.c
+++ app/myfunc.c
@@ -6,24 +6,22 @@
 
 
 // Функция для вычисления корней квадратного уравнения
-double formula(double a, double b, double c) {
+void formula(double a, double b, double c, double* root1, double* root2) {
     if (a == 0) {
-        return -222; // Это не квадратное уравнение, так как a = 0
+        *root1= -222; // Это не квадратное уравнение, так как a = 0
     }
 
     double discriminant = b * b - 4 * a * c;
 
     if (discriminant > 0) {
-		struct Roots ret;
-        ret.root1 = (-b + sqrt(discriminant)) / (2 * a);
-        ret.root2 = (-b - sqrt(discriminant)) / (2 * a);
-		return ret; //  Уравнение имеет два корня: root1, root2
+
+        *root1 = (-b + sqrt(discriminant)) / (2 * a);
+        *root2 = (-b - sqrt(discriminant)) / (2 * a); //  Уравнение имеет два корня: root1, root2
 		
     } else if (discriminant == 0) {
-        double root = -b / (2 * a);
-		return root; // Уравнение имеет один корень
+        *root1 = -b / (2 * a); // Уравнение имеет один корень
 		
     } else {
-		return -111; // Уравнение не имеет вещественных корней, так как дискриминант меньше 0
+		*root1 = -111; // Уравнение не имеет вещественных корней, так как дискриминант меньше 0
     }
 }
