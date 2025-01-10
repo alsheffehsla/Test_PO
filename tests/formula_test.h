6d9990d63449ad56586ff1ee5d6e52ce691063b1
alshef <alsheffehsla@yandex.ru>
2025-01-10 17:51:35 +0300

    исправлены ошибки в тестах

--- tests/formula_test.h
+++ tests/formula_test.h
@@ -9,24 +9,28 @@
 extern "C" {
 #include "myfunc.h"
 }
-double root_1=0;
-double root_2=0;
+double root_1 = 0;
+double root_2 = 0;
 
 
 TEST(formulaTest, a0) {
-    formula(0,2,4,&root_1, &root_2);
-    ASSERT_EQ(root_1, -222); // Это не квадратное уравнение, так как a = 0
+    formula(0,2,4, &root_1, &root_2);
     root_1=0;
+    root_2=0;
+    ASSERT_EQ(root_2, 0); // Это не квадратное уравнение, так как a = 0
+    ASSERT_EQ(root_1, 0);
+    root_1=0;
+    root_2=0;
 }
 
 TEST(formulaTest, 2_roots) {
-    formula(1,2,3,&root_1, &root_2);
+    formula(1,2,0,&root_1, &root_2);
     ASSERT_EQ(root_1, 0);
     ASSERT_EQ(root_2,-2);
     root_1=0;
     root_2=0;
     formula(2,5,3,&root_1, &root_2);
-    ASSERT_EQ(root_1, 1);
+    ASSERT_EQ(root_1, -1);
     ASSERT_EQ(root_2,-1.5);
     root_1=0;
     root_2=0;
@@ -34,13 +38,16 @@ TEST(formulaTest, 2_roots) {
 
 TEST(formulaTest, 1_root) {
     formula(4,4,1,&root_1, &root_2);
-    ASSERT_EQ(root_1, -2);
+    ASSERT_EQ(root_1, -0.5);
     root_1=0;
 }
 
 TEST(formulaTest, 0_root) {
     formula(2,0,1,&root_1, &root_2);
-    ASSERT_EQ(root_1, -111);
+    ASSERT_EQ(root_1, 0);
+    ASSERT_EQ(root_2, 0);
     root_1=0;
+    root_2=0;
 }
+
 #endif // FORMULA_H
